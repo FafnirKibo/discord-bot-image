9982b64bc89400aa65bf84150fb69642c4cd611c
FafnirKibo <mateohymonet@hotmail.fr>
2022-03-27 13:51:34 +0200

    add rename

--- main.py
+++ main.py
@@ -330,6 +330,39 @@ async def move(ctx, ancientFolder, ancientName, animeName):
 
 
 @client.command()
+async def rename(ctx, ancientName, newName):
+    destination = destFolder + ancientName + separator
+    # Titre pour le embed
+    title = "Renommage de " + ancientName + " en " + newName
+    # On regarde si l'ancien dossier existe au moins
+    if os.path.exists(destination):
+        count = 1
+        imageName = destination + ancientName + " - " + str(count).zfill(3) + ".png"
+        # On regarde s'il y a au moins une image dans l'ancien dossier
+        if os.path.exists(imageName):
+            # On boucle sur les screen et on les renomme
+            while os.path.exists(imageName):
+                newImageName = destination + newName + " - " + str(int(count)).zfill(3) + ".png"
+                os.rename(imageName, newImageName)
+                count += 1
+                imageName = destination + ancientName + " - " + str(count).zfill(3) + ".png"
+            description = "Fin du renommage des screen"
+            await sendEmbed(ctx, title, description)
+
+            # Nouveau nom du dossier
+            newDest = destFolder + newName + separator
+            os.rename(destination, newDest)
+            description = "Fin du renommage du dossier"
+            await sendEmbed(ctx, title, description)
+        else:
+            description = "Aucun screen pour l'anime " + ancientName + " à renommer"
+            await sendEmbed(ctx, title, description)
+    else:
+        description = "Aucun dossier " + ancientName + " trouvé"
+        await sendEmbed(ctx, title, description)
+
+
+@client.command()
 async def random(ctx, *, animeName):
     destination = destFolder + animeName + separator
 
